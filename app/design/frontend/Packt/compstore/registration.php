e85494560eabeb3e76c91209277f347e97da6dc5
karimsamir <karim5977@gmail.com>
2017-10-24 20:51:17 +0200

    finished created a new theme compStore

--- app/design/frontend/Packt/compstore/registration.php
+++ app/design/frontend/Packt/compstore/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Packt/basic',
+    'frontend/Packt/compustore',
     __DIR__
 );
